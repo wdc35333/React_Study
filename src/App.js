b9bb669ff791b02a2637ddd948184c7459a68968
원동찬 <37140854+wdc35333@users.noreply.github.com>
2022-09-27 17:32:23 +0900

    0927

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import MyComponent from "./MyComponent";
-import PracticeComponent from "./PracticeComponent";
+import Counter from "./Counter";
+// import MyComponent from "./MyComponent";
+// import PracticeComponent from "./PracticeComponent";
 const App = () => {
   return (
     <div>
-      <MyComponent name={"React"} favoriteNumber={1}>
+      {/* <MyComponent name={"React"} favoriteNumber={1}>
         리액트
       </MyComponent>
 
@@ -12,7 +13,8 @@ const App = () => {
         age={28}
         phone={73315350}
         address={"인천광역시 남동구 구월동 1093-8번지 채움 101호"}
-      ></PracticeComponent>
+      ></PracticeComponent> */}
+      <Counter />
     </div>
   );
 };
