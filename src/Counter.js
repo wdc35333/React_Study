caa550281dbc59753ac171766b3e18319bc3fb01
원동찬 <37140854+wdc35333@users.noreply.github.com>
2022-09-28 15:28:45 +0900

    setState 사용법

--- src/Counter.js
+++ src/Counter.js
@@ -16,7 +16,15 @@ class Counter extends Component {
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다.");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
